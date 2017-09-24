99ed24b1bc3e0b770a67597108081d8c41af6ba6
NathanielKBlan <nateblanquel@gmail.com>
2017-09-24 09:57:12 -0700

    Re-organized files on 9/24/17

--- PracticeForTest.java
+++ PracticeForTest.java
@@ -1,9 +1,26 @@
 class PracticeForTest{
   public static void main(String[] args){
     int i = 1;
+    int j = 0;
+    byte a = 123;
+    float g = 2.73F;
+    double b = 2.73;
+    float d = (float) b;
     while(i <= 100){
       System.out.println(i);
       i++;
     }
+    while(j <= 200){
+      switch(j % 5){
+        case 0:
+        System.out.println(j);
+        break;
+      }
+      j++;
+    }
+    System.out.println(a);
+    System.out.println(g);
+    System.out.println(b);
+    System.out.println(d);
   }
 }
