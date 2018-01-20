701ac05d51ada6652ce46d2c25557fd1609cd157
NathanielKBlan <nateblanquel@gmail.com>
2018-01-19 17:02:05 -0800

    Updated on 01/19/18

--- Assignments/Shapes/Main.java
+++ Assignments/Shapes/Main.java
@@ -6,14 +6,23 @@ public class Main{
 
     JFrame frame = new JFrame("Fun with shapes!");
 
-    Rectangle rect = new Rectangle(100, 100, 150, 200, 100, 172, 213);
+    Rectangle rect = new Rectangle(100, 100, 30, 60, 100, 172, 213);
+
+    for(int i = 0; i < 10; i++){
+      for(int j= 0; j < 10; j++){
+        rect = new Rectangle(i+40, j+40, 30, 30, 100, 172, 213);
+        frame.setContentPane(rect);
+      }
+    }
+    //rect.draw();
 
     Square sq = new Square(300, 300, 50, 123, 21, 211);
+    //sq.draw();
 
     Circle cl = new Circle(300, 300, 100, 210, 51, 25);
+    //c1.draw();
     //GraphicsPanel content = new GraphicsPanel();
 
-    frame.setContentPane(cl);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(800, 600);
     frame.setLocation(120,70);
