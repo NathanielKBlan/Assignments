3802a8d7759afabefbae2ed68b0f55d7cf289300
NathanielKBlan <nateblanquel@gmail.com>
2018-02-20 13:26:33 -0800

    Updated on 02/20/18

--- JME Practice/Hello JME/HelloJME3.java
+++ JME Practice/Hello JME/HelloJME3.java
@@ -7,6 +7,10 @@ import com.jme3.math.ColorRGBA;
 /** Sample 1 - how to get started with the most simple JME 3 application.
  * Display a blue 3D cube and view from all sides by
  * moving the mouse and pressing the WASD keys. */
+
+/*SimpleApplication is soon to be replaced by BaseAppliation to allow for modification of various game states without having
+* go through protected fields. */
+
 public class HelloJME3 extends SimpleApplication {
 
     public static void main(String[] args){
